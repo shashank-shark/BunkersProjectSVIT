42445a00a4a8e09342af04c37493d4e9f9987aac
Shashank J <shashankreddy231@gmail.com>
2018-08-22 11:08:36 +0530

    Gets records from the database

---
+++ GetResources.java
@@ -0,0 +1,55 @@
+package bunkersprojectsvit;
+import java.sql.*;
+/*
+ * I need a static file to access all the data members.
+ */
+
+public class GetResources {
+	
+	Connection myCon;
+	Statement myStat;
+	ResultSet myRes;
+	
+	/* declare the static members */
+	static String usn[] = new String[75];
+	static boolean present[] = new boolean[75];
+	static int attendedHours[] = new int[75];
+	static float percentageAtt[] = new float[75];
+	
+	/* Now let this file connect to the shortage list database */
+	public  void getCurrentStatus(int n)
+	{
+		int temphours;
+		float tempperc;
+		
+		GetDatabaseConnection conObj = new GetDatabaseConnection();
+		try
+		{
+			myCon = conObj.getMyConnection("totallist");
+			myStat = myCon.createStatement();
+			myRes = myStat.executeQuery("select * from attendinfo");
+			
+			/* I think i have successfully retrieved
+			 * the values from the shortage list database
+			 */
+			for (int i = 0; myRes.next() == true; i++)
+			{
+				//Now increment the number of hours and calculate the percentage
+				if (Bunkers_Automated_MS.absentOh[i] != 0)
+				{
+					temphours = myRes.getInt("nhours");
+					temphours = temphours + n;
+					myStat = myCon.createStatement();
+					myStat.executeUpdate("update attendinfo set nhours="+temphours + " where usn='" +Bunkers_Automated_MS.PresentUsn[i]+"'");
+				
+				}
+			}
+			
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.toString());
+		}
+		
+	}
+}
