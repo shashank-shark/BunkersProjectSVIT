fba6844dbb72c7de8898e8fa31722ec2dda9ff20
Shashank J <shashankreddy231@gmail.com>
2018-08-22 11:10:45 +0530

    Just a Dummy table for furthur development and implementation of new functionalities to project

---
+++ StoreAttenededTemp.java
@@ -0,0 +1,11 @@
+package bunkersprojectsvit;
+
+public class StoreAttenededTemp {
+	/* here i try to store the students who attended 
+	 * the classes for this session.
+	 */
+	public StoreAttenededTemp(boolean isAbsent[]) {
+		GetDatabaseConnection conObj = new GetDatabaseConnection();
+		conObj.getMyConnection("temp");
+	}
+}
