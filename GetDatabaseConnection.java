be1ffed948b379c59e3c2e522dfd767516015e0d
shashank-shark <35640091+shashank-shark@users.noreply.github.com>
2018-10-16 22:09:17 +0530

    Add files via upload
    Includes major changes for creation of automatic tables and stored procedures.

--- GetDatabaseConnection.java
+++ GetDatabaseConnection.java
@@ -8,6 +8,7 @@ public class GetDatabaseConnection {
 	private String url;
 	private String user="cse";
 	private String pass="Cse@1998";
+	private int flag = 0;
 	
 	private Connection myCon;
 	
@@ -17,9 +18,8 @@ public class GetDatabaseConnection {
 		
 		String sqlCreate = "create database if not exists totallist";
 		String useDatabase = "use totallist";
-		
 		/* Now create table totallist and i am going to create the table for the first time*/
-		String createIsPresent = "create table ispresent (tablepresent int)";
+		String createIsPresent = "create table if not exists ispresent (tablepresent int)";
 		String insertIsPresent = "insert into ispresent(tablepresent) values (0)";
 		
 		/* at last i am going to set the value of tablepresent to 1 */
@@ -28,37 +28,89 @@ public class GetDatabaseConnection {
 //		Now start creating the actual tables 
 		
 		/* Creating students table */
-		String createStudents = "create table students (usn varchar(20), name varchar(20), email varchar(20), phone varchar(20), primary key (usn))";
+		String createStudents = "create table if not exists students (usn varchar(20), name varchar(20), email varchar(20), phone varchar(20), primary key (usn))";
 				
 		/* create tabel attendinfo */
-		String createAttendInfo = "create table attendinfo (usn varchar(20),name varchar(20),nhours int,email varchar(20),primary key (usn))";
+		String createAttendInfo = "create table if not exists attendinfo (usn varchar(20),name varchar(20),nhours int,email varchar(20),primary key (usn))";
 		
 		/* create table shortagelist */
-		String createShortageList = "create table shortagelist (usn varchar(20), name varchar(20), percent float, primary key (usn))";
+		String createShortageList = "create table if not exists shortagelist (usn varchar(20), name varchar(20), percent float, primary key (usn))";
 		
 		/* create table mailsent */
-		String createMailSent = "create table mailsent (usn varchar(20))";
+		String createMailSent = "create table if not exists mailsent (usn varchar(20))";
 		
 		/* create table temp */
-		String createTemp = "create table temp (usn varchar(20),name varchar(20))";
+		String createTemp = "create table if not exists temp (usn varchar(20),name varchar(20))";
 		
 		/* Now update the values as we created the tables */
 		String updateTablepresent = "update ispresent set tablepresent=1 where tablepresent=0";
 		
-		
-		
-	
 		/* I am going to fix the constants for password */
-		url = dataBaseName;
+		url = "totallist";
 		
 		/* Now get the connection */
-		url = "jdbc:mysql://localhost:3306/" + dataBaseName + "?useSSL=false";
+		url = "jdbc:mysql://localhost:3306/totallist?useSSL=false";
 		try {
 			myCon = DriverManager.getConnection(url, user, pass);
+			System.out.println("Successfully connected");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		/* Return the connection */
+		
+		/* Now check whether the database is created or not */
+		try {
+			Statement myStat = myCon.createStatement();
+			/* select the tablepresent from ispresnt */
+//			ResultSet myRes = myStat.executeQuery("select * from ispresent");
+			myStat.execute(sqlCreate);
+			myStat.execute(useDatabase);
+			myStat.execute(createIsPresent);
+			myStat.execute(insertIsPresent);
+			myStat.execute(createStudents);
+			myStat.execute(createAttendInfo);
+			myStat.execute(createShortageList);
+			myStat.execute(createMailSent);
+			myStat.execute(createTemp);
+			myStat.execute(updateTablepresent);
+			
+			/* Now check whether the database is created or not */
+//			while (myRes.next())
+//			{
+//				flag = myRes.getInt("tablepresent");
+//			}
+//			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 		return myCon;
+		
+//		if (flag == 1)
+//		{
+			
+//		}
+//		else
+//		{
+//			try {
+////				Statement myStat = null;
+////				myStat.execute(sqlCreate);
+////				myStat.execute(useDatabase);
+////				myStat.execute(createIsPresent);
+////				myStat.execute(insertIsPresent);
+////				myStat.execute(createStudents);
+////				myStat.execute(createAttendInfo);
+////				myStat.execute(createShortageList);
+////				myStat.execute(createMailSent);
+////				myStat.execute(createTemp);
+////				myStat.execute(updateTablepresent);
+//			} catch (SQLException e) {
+//				e.printStackTrace();
+//			}
+//		}
+		
+		
+		
+		/* Return the connection */
+	
 	}
 }
