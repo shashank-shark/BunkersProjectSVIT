72d204528092f870ec318c26087d295994e513eb
shashank-shark <shashankreddy231@gmail.com>
2018-11-13 15:28:23 +0530

    Establishes connection with the database

--- GetDatabaseConnection.java
+++ GetDatabaseConnection.java
@@ -28,6 +28,21 @@ public class GetDatabaseConnection {
 //		Now start creating the actual tables 
 		
 		/* Creating students table */
+<<<<<<< HEAD
+		String createStudents = "create table if not exists students (usn varchar(60), name varchar(60), email varchar(60), phone varchar(60), primary key (usn))";
+				
+		/* create tabel attendinfo */
+		String createAttendInfo = "create table if not exists attendinfo (usn varchar(60),name varchar(60),nhours int,email varchar(60),foreign key (usn) references students (usn) on delete cascade)";
+		
+		/* create table shortagelist */
+		String createShortageList = "create table if not exists shortagelist (usn varchar(60), name varchar(60), percent float, foreign key (usn) references students (usn) on delete cascade)";
+		
+		/* create table mailsent */
+		String createMailSent = "create table if not exists mailsent (usn varchar(60), sentdate date, senttime datetime, foreign key (usn) references students (usn) on delete cascade)";
+		
+		/* create table temp */
+		String createTemp = "create table if not exists temp (usn varchar(60),name varchar(60), foreign key (usn) references students (usn) on delete cascade)";
+=======
 		String createStudents = "create table if not exists students (usn varchar(20), name varchar(20), email varchar(20), phone varchar(20), primary key (usn))";
 				
 		/* create tabel attendinfo */
@@ -41,10 +56,29 @@ public class GetDatabaseConnection {
 		
 		/* create table temp */
 		String createTemp = "create table if not exists temp (usn varchar(20),name varchar(20))";
+>>>>>>> 0560b61d1abc5362e13eee3c7ce12573709d6b93
 		
 		/* Now update the values as we created the tables */
 		String updateTablepresent = "update ispresent set tablepresent=1 where tablepresent=0";
 		
+<<<<<<< HEAD
+		/* Inserting a trigger here */
+//		String delimeter = "DELIMITER //";
+//		String createTrigger = "create trigger insert_date_and_time after insert on mailsent for each row ";
+//		String beginTrigger = "BEGIN ";
+//		String mainAction1 = "set sentdate=CURDATE() ";
+//		String mainAction2 = "set senttime=NOW() ";
+//		String endTrigger = "END";
+		
+//		String finalTrigger = "create trigger insert_date_and_time after insert on mailsent for each row begin update mailsent set sentdate=CURDATE(),senttime=NOW() end;";
+			
+//		triggerBuilder.append(" CREATE TRIGGER insert_associated_inquiry BEFORE UPDATE ON inquiry ");
+		
+	
+		
+		
+=======
+>>>>>>> 0560b61d1abc5362e13eee3c7ce12573709d6b93
 		/* I am going to fix the constants for password */
 		url = "totallist";
 		
@@ -73,6 +107,34 @@ public class GetDatabaseConnection {
 			myStat.execute(createTemp);
 			myStat.execute(updateTablepresent);
 			
+<<<<<<< HEAD
+			
+			myStat.execute("drop procedure if exists get_no_of_emails");
+			
+			/* Now create stored procedure in the database */
+			myStat.execute("create definer=`cse`@`localhost` procedure `get_no_of_emails`(IN the_usn varchar(20))"+ " begin"+" insert into mailsent (usn,sentdate,senttime) values (the_usn,null,null);"+" end");
+			
+			
+			/* Start creating trigger now */
+//			myStat.execute(delimeter);
+//			myStat.execute(createTrigger);
+//			myStat.execute(beginTrigger);
+//			myStat.execute(mainAction1);
+//			myStat.execute(mainAction2);
+//			myStat.execute(endTrigger);
+//			myStat.execute(finalTrigger);
+			myStat.execute("DROP TRIGGER IF EXISTS insert_date_and_time");
+			myStat.execute("CREATE TRIGGER insert_date_and_time BEFORE INSERT ON mailsent " //
+			        + "FOR EACH ROW "//
+			        + "BEGIN "//
+			        + "set NEW.sentdate=CURDATE(),NEW.senttime=NOW(); "//
+			        + "END;");
+			
+			
+			/* trying to create trigger in the database */
+
+=======
+>>>>>>> 0560b61d1abc5362e13eee3c7ce12573709d6b93
 			/* Now check whether the database is created or not */
 //			while (myRes.next())
 //			{
