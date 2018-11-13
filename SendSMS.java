d2102e2c1347e37d8a15826933852fb2272fc732
shashank-shark <shashankreddy231@gmail.com>
2018-11-13 15:29:36 +0530

    Program to send SMS alerts

--- SendSMS.java
+++ SendSMS.java
@@ -1,54 +1,38 @@
-package bunkersprojectsvit;
-
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLEncoder;
+
+import javax.swing.JOptionPane;
 
 public class SendSMS {
 	public void sendSMSTo(String getNumber, String messageFrom)
 	{
 		try {
-
-			String recipient = getNumber;
-
-			String message = messageFrom;
-
-			String username = "admin";
-
-			String password = "abc123";
-
-			String originator = "+919900767621";
-
-			String requestUrl  = "http://127.0.0.1:9501/api?action=sendmessage&" +
-
-			 "username=" + URLEncoder.encode(username, "UTF-8") +
-
-			 "&password=" + URLEncoder.encode(password, "UTF-8") +
-
-			 "&recipient=" + URLEncoder.encode(recipient, "UTF-8") +
-
-			 "&messagetype=SMS:TEXT" +
-
-			 "&messagedata=" + URLEncoder.encode(message, "UTF-8") +
-
-			 "&originator=" + URLEncoder.encode(originator, "UTF-8") +
-
-			 "&serviceprovider=GSMModem1" +
-
-			 "&responseformat=html";
-			
-			URL url = new URL(requestUrl);
-
-			HttpURLConnection uc = (HttpURLConnection)url.openConnection();
-
-			System.out.println(uc.getResponseMessage());
-
-			uc.disconnect();
-
-			}
-			catch (Exception e)
-			{
-				System.out.println(e.toString());
-			}
+			String myurl = "https://smsapi.engineeringtgr.com/send/?Mobile=9380021471&Password=lordvilgax&Message="+messageFrom+"&To="+getNumber+"&Key=www.szoFRB9jAMbcskN4vhiUQWES7";
+//			https://smsapi.engineeringtgr.com/send/?Mobile=&Password=&Message=&To=&Key=
+				
+			System.out.println(myurl);
+            URL url = new URL(myurl);
+            URLConnection urlcon = url.openConnection();
+            InputStream stream = urlcon.getInputStream();
+            int i;
+            String response="";
+            while ((i = stream.read()) != -1) {
+                response+=(char)i;
+            }
+            if(response.contains("success")){
+                System.out.println("Successfully send SMS");
+                JOptionPane.showMessageDialog(null, "Message successfully sent");
+                //your code when message send success
+            }else{
+                System.out.println(response);
+                //your code when message not send
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 	}
 }
